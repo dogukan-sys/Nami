b220ae6198cb5169f048281fa4f944af5b8d361c
dogukan.karademir <dogukan.karademir@rhenus.com>
2023-12-11 16:08:01 +0100

    Update config.js with new settings

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,17 @@
 const config = {
+  // General
+  name: "John",
+  imageBackground: false,
+  openInNewTab: true,
+  twelveHourFormat: false,
+
+  // Greetings
+  greetingMorning: "Good morning!",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Go to Sleep!",
+
+  // Weather
   api_key: "c92f9055183219e4888793384adaa09e",
   city: "dortmund",
   units: "metric"
