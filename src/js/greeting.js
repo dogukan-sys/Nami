7cf5dc8cced2515f48dfd0ca1e64d7bb15d13bd2
dogukan <Karademird67@gmail.com>
2023-12-24 05:02:19 +0100

    fix greetings

--- src/js/greeting.js
+++ src/js/greeting.js
@@ -10,14 +10,14 @@ const greeting2 = config.greetingAfternoon;
 const greeting3 = config.greetingEvening;
 const greeting4 = config.greetingNight;
 
-if (hour >= 23 || hour < 6) {
-  setGreeting(greeting1);
-} else if (hour >= 6 && hour < 12) {
-  setGreeting(greeting2);
-} else if (hour >= 12 && hour < 17) {
-  setGreeting(greeting3);
-} else {
+if (hour >= 22 || hour < 6) {
   setGreeting(greeting4);
+} else if (hour >= 6 && hour < 12) {
+  setGreeting(greeting1);
+} else if (hour >= 12 && hour < 17) {
+  setGreeting(greeting2);
+} else {
+  setGreeting(greeting3);
 }
 function setGreeting(greet) {
   greeting.innerText = `${greet} ${name}`;
