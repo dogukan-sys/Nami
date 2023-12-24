ef6ba2567dffa7e48c27672d8c01cd1393a0c6bf
dogukan <Karademird67@gmail.com>
2023-12-24 05:02:11 +0100

    use one call api

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,24 +1,27 @@
 import { config } from "./config";
 
-exportWeatherData();
-
+handleCurrentWeatherData();
 async function getWeatherData() {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${config.city}&units=${config.units}&lang=de&appid=${config.api_key}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=	51.5142449&lon=7.468429&
+    &units=${config.units}&lang=de&appid=${config.api_key}`
   );
   const data = response.json();
   return data;
 }
 
-async function exportWeatherData() {
+async function handleCurrentWeatherData() {
   const data = await getWeatherData();
-  const temp = data.main.temp;
-  const description = data.weather[0].description;
-  const iconId = data.weather[0].icon;
-  setWeather(temp, description, iconId);
+
+  const currentTemp = data.current.temp;
+  const currentDescription = data.current.weather[0].description;
+  const currentIconId = data.current.weather[0].icon;
+
+  setCurrentWeather(currentTemp, currentDescription, currentIconId);
+  display(data);
 }
 
-function setWeather(temp, description, iconId) {
+function setCurrentWeather(temp, description, iconId) {
   const iconElement = document.querySelector(".weatherIcon");
   const tempElement = document.querySelector(".weatherValue p");
   const descElement = document.querySelector(".weatherDescription p");
@@ -26,4 +29,10 @@ function setWeather(temp, description, iconId) {
   iconElement.src = icon;
   tempElement.innerHTML = `${temp.toFixed(0)} - °<span class="g">c</span> `;
   descElement.innerHTML = description;
+}
+
+async function display(data) {
+  // Console log the data in another function, because Promises ¯\_(ツ)_/¯
+  const log = await data;
+  console.log(log);
 }
